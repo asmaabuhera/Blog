f3245fdbd0781acfaac19fe5384e730bfb4981a5
asma <asmaaabuhera@joacademy.com>
2024-04-08 17:07:58 +0300

    edit link

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <h1 className="text-3xl p-8 text-center">Welcome to <b>BLOGS</b> where you can express your self! </h1>
       <h2 className="text-2xl p-4 font-semibold">Featured Posts </h2>
       <BlogList featuredBlogs={true}/>
-      <Link href="/blogs" className="text-xl underline hover:font-bold text-[#59a0a1]">See All Blogs </Link>
+      <Link href="/blogs" className="text-xl underline hover:font-bold text-[#59a0a1]">See All Blogs.. </Link>
     </main>
     
   );
